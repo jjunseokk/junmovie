21c90c6c941107ab9cc4780d0e9765fc8374c249
jjunseokk <87737169+jjunseokk@users.noreply.github.com>
2023-01-12 20:07:54 +0900

    Update MovieDetail.js

--- src/Pages/MovieDetail.js
+++ src/Pages/MovieDetail.js
@@ -38,7 +38,7 @@ function MyVerticallyCenteredModal(props) {
               height: "600",
               playerVars: {
                 autoplay: 1, //자동재생 O
-                rel: 0, //관련 동영상 표시하지 않음 (근데 별로 쓸모 없는듯..)
+                rel: 0, //관련 동영상 표시하지 않음
                 modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
               },
             }}
